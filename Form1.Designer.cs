3465b9f2ab1725a5880bf9529b5558d6fcc24d3e
MrGreen10 <erolbatu10@gmail.com>
2025-08-21 21:20:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace JuiceZIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.IO;
+using ZipFile = Ionic.Zip.ZipFile;
+using System.Diagnostics;
+
+namespace JuiceZIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@"C:/ZIP");
+            Directory.CreateDirectory(@"C:/ZIPSource");
+            Directory.CreateDirectory(@"C:/JuiceZIP");
+        }
+
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string extractPath = @"C:\JuiceZIP";
+
+            using (ZipFile zip = ZipFile.Read(textBox1.Text))
+            {
+                // Önce eski dosyaları temizle
+                if (Directory.Exists(extractPath))
+                {
+                    foreach (string file in Directory.GetFiles(extractPath))
+                        File.Delete(file);
+
+                    foreach (string dir in Directory.GetDirectories(extractPath))
+                        Directory.Delete(dir, true);
+                }
+
+                Directory.CreateDirectory(extractPath);
+
+                foreach (ZipEntry entry in zip)
+                {
+                    string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FileName));
+
+                    // Dizin kaçışını engelle
+                    if (!destinationPath.StartsWith(extractPath, StringComparison.OrdinalIgnoreCase))
+                    {
+                        MessageBox.Show($"Dizin kaçışı tespit edildi: {entry.FileName}", "JuiceZIP");
+                        continue; // Bu entry atlanacak
+                    }
+
+                    entry.Extract(extractPath, ExtractExistingFileAction.OverwriteSilently);
+                }
+
+                Process.Start(extractPath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string extractPath = @"C:\JuiceZIP";
+
+            using (ZipFile zip = ZipFile.Read(textBox1.Text))
+            {
+                if (Directory.Exists(extractPath))
+                    Directory.Delete(extractPath, true);
+
+                Directory.CreateDirectory(extractPath);
+
+                foreach (ZipEntry entry in zip)
+                {
+                    string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FileName));
+
+                    if (!destinationPath.StartsWith(extractPath, StringComparison.OrdinalIgnoreCase))
+                        continue;
+
+                    entry.Extract(extractPath, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(@"C:\ZIPSource");
+                zip.Save(@"C:/ZIP/JuiceZIP.zip");
+                Process.Start("C:/ZIP");
+                MessageBox.Show("Zip oluşturuldu!", "JuiceZIP");
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+
+        private void oluşturToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string extractPath = @"C:\JuiceZIP";
+            if (Directory.Exists(extractPath))
+                Directory.Delete(extractPath, true);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
